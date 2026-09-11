a5897ab03c3b15a2e01317dd0a9f574df193cfeb
Sandip kushwaha <163518436+sandip-kushwaha@users.noreply.github.com>
2026-09-11 14:54:42 +0545

    fix: remove fallback image URL in getImage function

--- src/pages/public/Featured.jsx
+++ src/pages/public/Featured.jsx
@@ -278,8 +278,7 @@ const Featured = () => {
 // IMAGE
 const getImage = (thumbnail) => {
   return (
-    thumbnail ||
-    "https://images.unsplash.com/photo-1504711434969-e33886168f5c?auto=format&fit=crop&w=1000&q=80"
+    thumbnail
   );
 };
 
