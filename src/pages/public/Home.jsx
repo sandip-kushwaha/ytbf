cb4060014bec4f808fc9d7987b32ea4817a3e03c
Sandip kushwaha <163518436+sandip-kushwaha@users.noreply.github.com>
2026-09-26 19:02:36 +0545

    fix: update link padding and text in Home component for better alignment

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -367,9 +367,9 @@ const Home = () => {
 
               <Link
                 to="/categories"
-                className="mt-6 inline-flex items-center gap-2 rounded-lg bg-blue-600 px-5 py-3 text-lg font-bold text-white transition hover:bg-blue-500"
+                className="mt-6 inline-flex items-center gap-2 rounded-lg bg-blue-600 px-4 py-3 text-lg font-bold text-white transition hover:bg-blue-500"
               >
-                सबै समाचारका बिषयहरु खोज्नुहोस्
+                सबै समाचारका बिषयहरु खोज्नुहोस् |
                 <ArrowRight size={20} />
               </Link>
             </div>
