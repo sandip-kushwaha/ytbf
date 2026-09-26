9aa1433c82bdba8562eda4f43b643aa4d04e0ef9
Sandip kushwaha <163518436+sandip-kushwaha@users.noreply.github.com>
2026-09-26 18:59:59 +0545

    fix: adjust link text and icon size in Home component for improved visibility

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -369,8 +369,8 @@ const Home = () => {
                 to="/categories"
                 className="mt-6 inline-flex items-center gap-2 rounded-lg bg-blue-600 px-5 py-3 text-lg font-bold text-white transition hover:bg-blue-500"
               >
-                सबै समाचारका बिषयहरु खोज्नुहोस् |
-                <ArrowRight size={16} />
+                सबै समाचारका बिषयहरु खोज्नुहोस्
+                <ArrowRight size={20} />
               </Link>
             </div>
 
